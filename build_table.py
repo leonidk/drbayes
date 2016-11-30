875e396d1c55767465263f11a71e42bcc6452497
leonidk <lkeselman@gmail.com>
2016-11-30 13:24:27 -0800

    always test both

--- build_table.py
+++ build_table.py
@@ -12,7 +12,6 @@ if __name__ == '__main__':
 
     min_freq = 0.001
     max_freq = 0.09
-    load_manual = False
     with open(items_filename,'rb') as fp:
         items = json.load(fp)
     data = []
@@ -21,7 +20,7 @@ if __name__ == '__main__':
         name, fb, mayo, wiki = item['fb']['name'], item['fb'], item['mayo'], item['wiki']
         wikiDesc = '\n'.join(wiki['mainSectionText'])
         if wikiDesc == None: wikiDesc = ""
-        data.append((name, '\n'.join(fb['desc']), '\n'.join(mayo['definition']), wikiDesc))
+        data.append((name.lower(), '\n'.join(fb['desc']), '\n'.join(mayo['definition']), wikiDesc))
     # extract symptoms
     for item in items:
         name, fb, mayo, wiki = item['fb']['name'], item['fb'], item['mayo'], item['wiki']
@@ -32,7 +31,7 @@ if __name__ == '__main__':
                 for paragraph in section[1]:
                     text = text + paragraph + u'\n'
             return text
-        data.append((name, '\n'.join(fb['sym']), '\n'.join(mayo['symptoms']), getTextFromWikiItem(wiki)))
+        data.append((name.lower(), '\n'.join(fb['sym']), '\n'.join(mayo['symptoms']), getTextFromWikiItem(wiki)))
 
     # extract independent data
     labels = [x[0] for x in data]
@@ -43,27 +42,6 @@ if __name__ == '__main__':
     # create boring data
     train_label = 2*labels
     train_data = fbV + mayoV
-
-    if load_manual:
-        with open(manual_filename,'rb') as fp:
-            md = json.load(fp)
-        test_label = []
-        test_data = []
-        for name in md:
-            test_label.append(name)
-            test_data.append(md[name])
-    else:
-        with open(wiki_filename,'rb') as fp:
-            wd = json.load(fp)
-        test_label = []
-        test_data = []
-        for data,name in wd:
-            test_label.append(name)
-            test_data.append(data)
-        #test_label = labels
-        #test_data = wikiDesc
-    
-    # get stop words
 
     #build tokenizer
     class Tokenizer:
@@ -91,18 +69,18 @@ if __name__ == '__main__':
             # remove alphanumeric
             res = [x for x in res if self.filter.match(x) != None]
             # stem
-            res2 = []
-            for x in res:
-                if x[-1] == 's': x = x[:-1]
-                if x[-3:] == 'ing' and self.vowels.match(x[:-3]) != None : x = x[:-3]
-                if x[-2:] == 'ed' and self.vowels.match(x[:-2]) != None : x = x[:-2]
-                if x[-2:] == 'ly' and self.vowels.match(x[:-2]) != None : x = x[:-2]
-                if x[-5:] == 'esses': x = x[:-5]
-                if x[-3:] == 'ies': x = x[:-2]
-                if x[-4:] == 'ness': x = x[:-4]
-                if x[-3:] == 'ful': x = x[:-3]
-                res2.append(x)
-            res = res2
+            #res2 = []
+            #for x in res:
+            #    if x[-1] == 's': x = x[:-1]
+            #    if x[-3:] == 'ing' and self.vowels.match(x[:-3]) != None : x = x[:-3]
+            #    if x[-2:] == 'ed' and self.vowels.match(x[:-2]) != None : x = x[:-2]
+            #    if x[-2:] == 'ly' and self.vowels.match(x[:-2]) != None : x = x[:-2]
+            #    if x[-5:] == 'esses': x = x[:-5]
+            #    if x[-3:] == 'ies': x = x[:-2]
+            #    if x[-4:] == 'ness': x = x[:-4]
+            #    if x[-3:] == 'ful': x = x[:-3]
+            #    res2.append(x)
+            #res = res2
             
             # remove stop
             res = [x for x in res if x not in self.stop]
@@ -146,8 +124,8 @@ if __name__ == '__main__':
     alphas = {}
     for c,v in prob.iteritems():
         for word,weight in v.iteritems():
-            prob[c][word] = (weight)/float(total_counts[c]+alpha*n)
-            alphas[c] = alpha/float(total_counts[c]+alpha*n)
+            prob[c][word] = (weight)/float(total_counts[c]+alpha*(n-len(v)))
+            alphas[c] = alpha/float(total_counts[c]+alpha*(n-len(v)))
     #theta = {c: {word: math.log((weight+alpha)/(total_counts[c]+alpha*n))\
     #            for word,weight in vec.iteritems()} \
     #            for c,vec in prob.iteritems()}
@@ -167,17 +145,35 @@ if __name__ == '__main__':
             #scores[c] = reduce(mul,(({k: v*(model[c][k]+alphas[c]) for k,v in vec.iteritems()}).values()))
         scores = sorted([(v,k) for k,v in scores.iteritems()])[::-1]
         return scores
-    top_1 = 0
-    top_5 = 0
-    total = 0
-    for lbl,data in zip(test_label,test_data):
-        scores = returnScores(data,tkn,theta,idf,alphas)
-        top5_lbl = [x[1] for x in scores[:5]]
-        if lbl == top5_lbl[0]:
-            top_1+=1
-        if lbl in top5_lbl:
-            top_5+=1
-        total+=1
-    print "{0:.2f}\t{1:.2f}".format(float(top_1)/total,float(top_5)/total)
+    for load_manual in [True,False]:
+        if load_manual:
+            with open(manual_filename,'rb') as fp:
+                md = json.load(fp)
+            test_label = []
+            test_data = []
+            for name in md:
+                test_label.append(name.lower())
+                test_data.append(md[name])
+        else:
+            with open(wiki_filename,'rb') as fp:
+                wd = json.load(fp)
+            test_label = []
+            test_data = []
+            for data,name in wd:
+                test_label.append(name.lower())
+                test_data.append(data)
+        top_1 = 0
+        top_5 = 0
+        total = 0
+        for lbl,data in zip(test_label,test_data):
+                scores = returnScores(data,tkn,theta,idf,alphas)
+                top5_lbl = [x[1] for x in scores[:5]]
+                if lbl == top5_lbl[0]:
+                    top_1+=1
+                if lbl in top5_lbl:
+                    top_5+=1
+                total+=1
+        dataset = 'manual' if load_manual else 'wiki'
+        print "{2}\t{0:.2f}\t{1:.2f}".format(float(top_1)/total,float(top_5)/total,dataset)
 
 
